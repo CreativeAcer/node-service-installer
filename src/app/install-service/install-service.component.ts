3f0c1dc751f66cb2e456e28a4a414c6e834f9f17
CreativeAcer <marcomoris@hotmail.com>
2019-09-25 20:58:45 +0200

    added open file for install script

--- src/app/install-service/install-service.component.ts
+++ src/app/install-service/install-service.component.ts
@@ -24,16 +24,32 @@ export class InstallServiceComponent implements OnInit {
   install(){
     // this.windowsservice.install('testService', 'service pure for testing',  'D:\\dev\\GitHub\\ServiceInstaller\\src\\assets\\HelloWorld.js');
     // Some data that will be sent to the main process
-    let Data = {
-      name: 'testService',
-      description: 'service pure for testing',
-      script: 'D:\\dev\\GitHub\\ServiceInstaller\\src\\assets\\HelloWorld.js'
-    };
-
-    // Send information to the main process
-    // if a listener has been set, then the main process
-    // will react to the request !
-    this.electronService.ipcRenderer.send('InstallService', Data);
+    this.electronService.remote.dialog.showOpenDialog(this.electronService.remote.getCurrentWindow(), {
+      filters: [{
+        name: 'Scripts',
+        extensions: []
+      }],
+      properties: ['openFile']
+    },
+    function (filepaths, bookmarks) {
+      if (filepaths === undefined) {
+        return;
+      }
+      if (filepaths && filepaths[0]) {
+        let Data = {
+          name: 'testService',
+          description: 'service pure for testing',
+          script: filepaths[0]
+        };
+    
+        // demo script 'D:\\dev\\GitHub\\ServiceInstaller\\src\\assets\\HelloWorld.js'
+        // Send information to the main process
+        // if a listener has been set, then the main process
+        // will react to the request !
+        this.electronService.ipcRenderer.send('InstallService', Data);
+      }
+      return;
+    }.bind(this));
   }
 
 }
