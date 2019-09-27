c688f159936ff15c8163033b80c2d889e3d3735b
CreativeAcer <marcomoris@hotmail.com>
2019-09-27 13:10:08 +0200

    translate update shift arount some components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     HeaderComponent, FooterComponent, SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   exports: [
     HeaderComponent, FooterComponent, SidebarComponent
